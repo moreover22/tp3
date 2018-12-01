b0cb546842667dc3132380ea240d3708b9313328
Agustín More <amore@fi.uba.ar>
2018-12-01 15:49:38 -0300

    Arreglo problema con nombre variables en grafo

--- grafo.py
+++ grafo.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from random import random
 class Grafo:
     def __init__(self):
         self.adyacencias = {}
@@ -10,38 +11,40 @@ class Grafo:
 
     # Hay que hacerlo dirigido o no dirigido?
     def sacar_vertice(self, vertice):
-        if vertice not in self.adyacentes:
+        if vertice not in self.adyacencias:
             return
-        for v in self.adyacentes.keys():
+        for v in self.adyacencias.keys():
             if not estan_conectados(self, v, vertice):
                 continue
             sacar_arista(self, v, vertice)
 
-        self.adyacentes.pop(vertice)
+        self.adyacencias.pop(vertice)
 
     # Hay que hacerlo dirigido o no dirigido?
     def agregar_arista(self, vertice_orig, vertice_dest, costo):
-        if vertice_orig not in self.adyacentes:
+        if vertice_orig not in self.adyacencias:
             return
-        self.adyacentes[vertice_orig][vertice_dest] = costo
+        self.adyacencias[vertice_orig][vertice_dest] = costo
 
 
     def sacar_arista(self, vertice_orig, vertice_dest):
-        if vertice_orig not in self.adyacentes:
+        if vertice_orig not in self.adyacencias:
             return
-        return self.adyacentes[vertice_orig].pop(vertice_dest)
+        return self.adyacencias[vertice_orig].pop(vertice_dest)
 
     # Hay que hacerlo dirigido o no dirigido?
     def estan_conectados(self, vertice_orig, vertice_dest):
-        return vertice_orig in self.adyacentes and \
-            vertice_dest in self.adyacentes[vertice_orig]
+        return vertice_orig in self.adyacencias and \
+            vertice_dest in self.adyacencias[vertice_orig]
 
     def adyacentes(self, vertice):
-        if vertice not in self.adyacentes:
+        if vertice not in self.adyacencias:
             return
-        return self.adyacentes[vertive]
+        return self.adyacencias[vertive]
 
     def peso(self, vertice_orig, vertice_dest):
         if not estan_conectados(self, vertice_orig, vertice_dest)
             return
-        return self.adyacentes[vertice_orig][vertice_dest]
+        return self.adyacencias[vertice_orig][vertice_dest]
+    def obtener_vertice(self):
+        return random(self.adyacencias.keys())
